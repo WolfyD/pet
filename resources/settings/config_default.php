6ac5cba79750f0ce35675afe39216cd2c6318c9b
MartonRadics <radics.marton.j@gmail.com>
2025-01-10 12:34:49 +0100

    Create config_default.php

---
+++ resources/settings/config_default.php
@@ -0,0 +1,14 @@
+<?php
+
+    // Rename this file config.php and add your own values for the database connection
+
+    namespace PET\settings;
+    // -- Database variables
+    define("DB_ADDRESS",    "127.0.0.1");
+    define("DB_USER",       "root");
+    define("DB_PASSWORD",   "root");
+    define("DB_PORT",       "3306");
+    define("DB_NAME",       "martonra_pet");
+
+    // -- Default variables
+    define("LIST_ROW_COUNT",    "22");
